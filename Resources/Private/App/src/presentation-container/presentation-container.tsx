b3aaf07c2bd1d6fdde8a0eb20bb1d0cfb08542a0
Stephan Schuler <stephan.schuler@netlogix.de>
2018-05-24 01:27:02 +0200

    Toggle slides on keypress

--- Resources/Private/App/src/presentation-container/presentation-container.tsx
+++ Resources/Private/App/src/presentation-container/presentation-container.tsx
@@ -1,4 +1,4 @@
-import { Component, Method, Prop, State } from '@stencil/core';
+import { Component, Listen, Method, Prop, State } from '@stencil/core';
 import { PresentationSlide } from "../presentation-slide/presentation-slide";
 
 @Component({
@@ -18,10 +18,6 @@ export class PresentationContainer {
         this.loading = false;
     }
 
-    render() {
-        return <slot/>;
-    }
-
     @Method()
     registerSlide(slide: PresentationSlide) {
         this.slides.push(slide);
@@ -33,5 +29,41 @@ export class PresentationContainer {
     @Method()
     unregisterSlide(delinquent: PresentationSlide) {
         this.slides = this.slides.filter(slide => slide !== delinquent);
+    }
+
+    @Listen('body:keydown.right')
+    @Listen('body:keydown.down')
+    nextSlide(ev: KeyboardEvent) {
+        ev.preventDefault();
+        this.activateOffset(this.currentOffset + 1);
+    }
+
+    @Listen('body:keydown.left')
+    @Listen('body:keydown.up')
+    previousSlide(ev: KeyboardEvent) {
+        ev.preventDefault();
+        this.activateOffset(this.currentOffset - 1);
+    }
+
+    protected activateOffset(nextOffset: number) {
+        if (nextOffset >= 0 && nextOffset < this.slides.length) {
+            if (this.activeSlide) {
+                this.activeSlide.active = false;
+            }
+            this.slides[nextOffset].active = true;
+        }
+    }
+
+    get currentOffset() {
+        return this.slides.indexOf(this.activeSlide);
+    }
+
+    get activeSlide(): PresentationSlide {
+        const active = this.slides.filter(slide => slide.active);
+        return active.length === 0 ? null : active[0];
+    };
+
+    render() {
+        return <slot/>;
     }
 }
