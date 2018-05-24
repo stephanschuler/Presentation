e11e8bdf1ab6b38e8dedec38ce7eb18e9b465d4b
Stephan Schuler <stephan.schuler@netlogix.de>
2018-05-24 02:16:39 +0200

    Move StaticResourceRequestHandler into src folder

--- demo/index.php
+++ demo/index.php
@@ -6,7 +6,7 @@ use StephanSchuler\Presentation\Demo\StaticResourceRequestHandler;
 
 /** @var ClassLoader $autoloader */
 $autoloader = require_once __DIR__ . '/../vendor/autoload.php';
-$autoloader->addPsr4('StephanSchuler\\Presentation\\Demo\\', __DIR__, true);
+$autoloader->addPsr4('StephanSchuler\\Presentation\\Demo\\', __DIR__ . '/src', true);
 
 $static = new StaticResourceRequestHandler(__DIR__ . '/../Resources/Public/App');
 $static->returnFile($_SERVER['PHP_SELF']);
